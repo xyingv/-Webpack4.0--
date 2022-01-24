c547a248cbf4f5e86d4d48d9ff64202a806b6f75
xiaoy11 <xiaoy11@chinatelecom.cn>
2022-01-24 15:49:13 +0800

    splitChunksPlugin的使用

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-function getComponent(){
-    return import('lodash').then(({default:_})=>{
-        var element=document.createElement('div');
-        element.innerHTML=_.join(['dell','lee'],'---')
-        return element;
-    })
-}
-getComponent().then(element=>{
-    document.body.appendChild(element)
-})
+import test from './test.js'
+console.log(test.name);
+
+// function getComponent(){
+//     return import(/* webpackChunkName:"lodash" */ 'lodash').then(({default:_})=>{
+//         var element=document.createElement('div');
+//         element.innerHTML=_.join(['dell','lee'],'---')
+//         return element;
+//     })
+// }
+// getComponent().then(element=>{
+//     document.body.appendChild(element)
+// })
