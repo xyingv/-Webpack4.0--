20d7d23a18247d0c517ae87120bafcc48274ab21
xiaoy11 <xiaoy11@chinatelecom.cn>
2022-01-25 14:57:56 +0800

    代码分析与prefetch与preloading

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -15,20 +15,20 @@ module.exports={
     optimization:{
         splitChunks:{
             chunks:'all', //共用类库时自动代码分割出来
-            minSize:0,
-            maxSize:50000,//5kb,loadsh 1mb =>会进行二次拆分，maxSize配置得较少
-            minChunks:2,//引入至少2次才会进行代码分割
-            maxAsyncRequests:5,//打开网页时只能加载5个请求，超出5个不会加载了
-            maxInitialRequests:3,//入口文件引入其他库，最多3个
-            automaticNameDelimiter:'~',//文件生成连接符
-            cacheGroups:{
-                vendors:false,//异步代码分割时命名前不会带vendors
-                default:{
-                    priority: -20,//优先级等级
-                    reuseExistingChunk:true,//打包a,b a引入b，打包时再用到b，那么只会复用不会再次打包b到common.js里了
-                    filename:'common.js'
-                }
-            }
+            // minSize:0,
+            // maxSize:50000,//5kb,loadsh 1mb =>会进行二次拆分，maxSize配置得较少
+            // minChunks:2,//引入至少2次才会进行代码分割
+            // maxAsyncRequests:5,//打开网页时只能加载5个请求，超出5个不会加载了
+            // maxInitialRequests:3,//入口文件引入其他库，最多3个
+            // automaticNameDelimiter:'~',//文件生成连接符
+            // cacheGroups:{
+            //     vendors:false,//异步代码分割时命名前不会带vendors
+            //     default:{
+            //         priority: -20,//优先级等级
+            //         reuseExistingChunk:true,//打包a,b a引入b，打包时再用到b，那么只会复用不会再次打包b到common.js里了
+            //         filename:'common.js'
+            //     }
+            // }
         }
     },
     output: {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-//懒加载
-async function getComponent(){
-    const {default:_}=await import(/* webpackChunkName:"lodash" */ 'lodash')
-    const element=document.createElement('div');
-    element.innerHTML=_.join(['dell','lee'],'---')
-    return element;
-}
 document.addEventListener('click',()=>{
-    getComponent().then(element=>{
-        document.body.appendChild(element)
+    import(/* webpackPrefetch: true */ './click.js').then(({default:_})=>{
+        func()
     })
-})
+}) 
+
